6eb268aea5354802c6790c88f4895995c1a376e2
Ale <alearanibar@gmail.com>
2020-11-29 22:14:57 -0800

    message project working on cors conflicts

--- client/src/hooks/localStorage.js
+++ client/src/hooks/localStorage.js
@@ -1,23 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const PREFIX = 'whatsapp-clone-'
+const PREFIX = "whatsapp-clone-";
 
-export default function useLocalStorage(key, initialValue){
-    const prefixedKey = PREFIX + key
-    const [value, setValue] = useState(() => {
-        const jsonValue = localStorage.getItem(prefixedKey)
-        if(jsonValue != null) return JSON.parse(jsonValue)
-        if(typeof initialValue === 'function') {
-            return initialValue()
-        } else {
-            return initialValue
-        }
-    })
+export default function useLocalStorage(key, initialValue) {
+  const prefixedKey = PREFIX + key;
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (typeof initialValue === "function") {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
 
-    useEffect(() => {
-        localStorage.setItem(prefixedKey, JSON.stringify(value))
-    }, [prefixedKey, value])
-    
-    return [value, setValue]
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  return [value, setValue];
 }
-
